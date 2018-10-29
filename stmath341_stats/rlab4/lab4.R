ef1e67bdf499391a09795613d3a8a5fc6acb8d22
Ryan Peters <petersryan84@gmail.com>
2018-10-29 08:52:46 -0700

    committing

--- stmath341_stats/rlab4/lab4.R
+++
@@ -1,9 +0,0 @@
-df_name <- 'kobe'
-# instead of forcing the script to reload the dataframe every time we run it, lets do a quick check to see if it's already
-# been instantiated from a previsou run
-if (exists(df_name)) is.data.frame(get(df_name)) else{
-  download.file("http://www.openintro.org/stat/data/kobe.RData", destfile = "kobe.RData")
-  load("kobe.RData")
-}
-
-head(kobe)
